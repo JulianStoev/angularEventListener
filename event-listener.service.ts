a8b6271b01782b2cd8646bdd6f26c27d0d77eeeb
Julian <48083546+JulianStoev@users.noreply.github.com>
2021-02-21 17:39:38 +0200

    Update event-listener.service.ts

--- event-listener.service.ts
+++ event-listener.service.ts
@@ -3,16 +3,16 @@ import { Injectable, NgZone } from '@angular/core';
 interface listenInterface {
   type: string;
   name: string;
-  callback: Function;
+  callback: (arg0?: any) => void;
   opts?: any;
-  el?: HTMLElement|Window|Document;
+  el?: HTMLElement| Window| Document;
 }
 
 interface removeInterface {
   type: string;
   name: string;
   opts?: any;
-  el?: HTMLElement|Window|Document;
+  el?: HTMLElement | Window | Document;
 }
 
 @Injectable({
@@ -25,7 +25,7 @@ export class EventListenerService {
   ) { }
 
   private data = {
-    action: (e:Event):void => {
+    action: (e: Event): void => {
       if (!e || !this.data.callbacks[e.type]) return;
       Object.keys(this.data.callbacks[e.type]).forEach(key => {
         if (typeof this.data.callbacks[e.type][key] == 'function') {
@@ -33,16 +33,16 @@ export class EventListenerService {
         }
       });
     },
-    callbacks: <any>{}
+    callbacks: {} as any
   };
 
-  protected list():void {
+  public list(): void {
     console.log(this.data.callbacks);
   }
 
-  public listen(data:listenInterface):void {
+  public listen(data: listenInterface): void {
     if (!this.data.callbacks[data.type]) {
-      this.data.callbacks[data.type] = <Function>{};
+      this.data.callbacks[data.type] = {} as () => void;
     }
     this.ngzone.runOutsideAngular(() => {
       (data.el || window).addEventListener(data.type, this.data.action, data.opts ? data.opts : {passive: true});
@@ -50,7 +50,7 @@ export class EventListenerService {
     this.data.callbacks[data.type][data.name] = data.callback;
   }
 
-  public remove(data:removeInterface):void {
+  public remove(data: removeInterface): void {
     if (this.data.callbacks[data.type] && this.data.callbacks[data.type][data.name]) {
       delete this.data.callbacks[data.type][data.name];
     }
